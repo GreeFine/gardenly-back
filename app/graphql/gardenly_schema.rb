8e3c7c158eba1c83f04b72968c250a22883a7361
Stormy Home <raphael.beltrand@gmail.com>
2018-12-06 18:57:51 +0100

    added graphql-errors, uniqueness of user emails, and user creation errors

--- app/graphql/gardenly_schema.rb
+++ app/graphql/gardenly_schema.rb
@@ -2,3 +2,25 @@ class GardenlySchema < GraphQL::Schema
   mutation(Types::MutationType)
   query(Types::QueryType)
 end
+
+GraphQL::Errors.configure(GardenlySchema) do
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    GraphQL::ExecutionError.new(exception.message.split('[').first)
+  end
+
+  rescue_from ActiveRecord::RecordInvalid do |exception|
+    GraphQL::ExecutionError.new(exception.record.errors.full_messages.join("\n"))
+  end
+
+  rescue_from ActiveRecord::StatementInvalid do |exception|
+    [GraphQL::ExecutionError.new(/ERROR:*(.*)/.match(exception.message)[0].remove("ERROR:  ").capitalize)]
+  end
+
+  rescue_from ActiveModel::RangeError do |exception|
+    [GraphQL::ExecutionError.new(exception.message)]
+  end
+
+  # rescue_from ::Pundit::NotAuthorizedError do |exception|
+  #   GraphQL::ExecutionError.new(exception.message);
+  # end
+end
