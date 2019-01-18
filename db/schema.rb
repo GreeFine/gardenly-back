20e63f604fe050b146031d25c9cd9f4b02ad4fa4
Stormy <raphael.beltrand@gmail.com>
2019-01-18 15:25:31 -0800

    reclone

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2018_11_29_142752) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
   enable_extension "uuid-ossp"
 
