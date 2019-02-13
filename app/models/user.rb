fc41126d6663e28a641b8c95da313ebd94549640
Raphaeël <raphou007@hotmail.fr>
2019-02-12 21:42:17 -0800

    Authenticity check and def, allow blank phone_number

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   self.primary_key = :uuid
 
   validates_uniqueness_of :email
-  validates_uniqueness_of :phone_number
+  validates_uniqueness_of :phone_number, :allow_blank => true
   validates_uniqueness_of :username
 
   has_many :sessions
