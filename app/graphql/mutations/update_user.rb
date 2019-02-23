00163be24a9169ea2788f0298dcc9568c00e5764
Raphaeël <raphou007@hotmail.fr>
2019-02-23 12:08:59 -0800

    Update user mutation to accept more changes

--- app/graphql/mutations/update_user.rb
+++ app/graphql/mutations/update_user.rb
@@ -1,33 +1,34 @@
 class Mutations::UpdateUser < Mutations::BaseMutation
-  argument :name, String, required: false
+  argument :first_name, String, required: false
+  argument :last_name, String, required: false
+  argument :email, String, required: false
+  argument :username, String, required: false
+  argument :password, String, required: false
   argument :age, Int, required: false
   argument :address, String, required: false
   argument :date_of_birth, String, required: false
   argument :phone_number, String, required: false
 
   field :user, Types::UserType, null: true
-  field :errors, [String], null: false
 
   def resolve(params)
     user = context[:current_user]
     if user.nil?
       return { errors: [ "Not connected" ] }
     end
-    user.name = params[:name]
+    user.last_name = params[:last_name]
+    user.first_name = params[:first_name]
+    user.username = params[:username]
+    user.password = params[:password]
     user.age = params[:age]
+    user.email = params[:email]
     user.address = params[:address]
     user.date_of_birth = params[:date_of_birth]
     user.phone_number = params[:phone_number]
 
-    if user.save
+    if user.save!
       {
-        user: user,
-        errors: [],
-      }
-    else
-      {
-        user: nil,
-        errors: session.errors.full_messages
+        user: user
       }
     end
   end
