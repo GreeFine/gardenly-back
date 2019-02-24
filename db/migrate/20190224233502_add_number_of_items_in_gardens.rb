ccd06ce4fe2cc58d1402567f51bddcbe9e1ace42
Stormy <raphael.beltrand@gmail.com>
2019-02-24 15:35:09 -0800

    nb of items in garden

---
+++ db/migrate/20190224233502_add_number_of_items_in_gardens.rb
@@ -0,0 +1,4 @@
+class AddNumberOfItemsInGardens < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_23_195852) do
+ActiveRecord::Schema.define(version: 2019_02_24_233502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
