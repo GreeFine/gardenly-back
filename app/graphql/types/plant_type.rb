f1654574698155a07f259e69ebe06cc94f81e6c4
Stormy <raphael.beltrand@gmail.com>
2019-05-04 11:48:04 -0700

    fix to plants and image queris

--- app/graphql/types/plant_type.rb
+++ app/graphql/types/plant_type.rb
@@ -4,5 +4,10 @@ module Types
     implements Types::ActiveRecordInterface
     field :id, ID, null: false
     field :name, String, null: false
+    field :photo, String, null: false
+
+    def photo
+      object.photo_url
+    end
   end
 end
