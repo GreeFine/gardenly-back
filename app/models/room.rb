7a9b2f16444526df1c6320ce1db4a21394e606b4
Raphael <greefine@hotmail.fr>
2019-05-04 17:20:19 -0700

    dependant destroy for rooms

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,7 +1,8 @@
+# frozen_string_literal: true
 
 class Room < ApplicationRecord
   self.primary_key = :uuid
 
-  has_many :messages
+  has_many :messages, dependent: :destroy
   has_and_belongs_to_many :users
 end
