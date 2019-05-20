d15cc0ff0e4f1aac287d4be44c257e31eb17314b
Raphael <greefine@hotmail.fr>
2019-05-20 14:44:25 -0700

    Update and use of Subscriptions for message type
    Debug: creating user add it to a default chat room

--- app/graphql/mutations/create_user.rb
+++ app/graphql/mutations/create_user.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Mutations::CreateUser < Mutations::BaseMutation
   argument :username, String, required: true
   argument :email, String, required: true
@@ -8,12 +10,18 @@ class Mutations::CreateUser < Mutations::BaseMutation
   field :user, Types::UserType, null: false
 
   def resolve(params)
-    if ! context[:current_session].nil?
-      return GraphQL::ExecutionError.new("User still connected")
+    unless context[:current_session].nil?
+      return GraphQL::ExecutionError.new('User still connected')
     end
+
     # TODO : check password is strong
     user = User.new(params)
-    user.save!()
+
+    # FIXME: Change public room for friends rooms
+    publicroom = Room.find('d69ab13a-1731-4d2b-9b62-246d8c4e463e')
+    user.rooms << publicroom
+
+    user.save!
     {
       user: user
     }
