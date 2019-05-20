3581500c7977782c6b5614d65a5e59066831f9b6
Raphael <greefine@hotmail.fr>
2019-05-19 22:38:49 -0700

    Add env for config Cable and Cors

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module Gardenly
 
     config.middleware.insert_before 0, Rack::Cors do
           allow do
-            origins 'localhost:3000'
+            origins ENV.fetch("HOST_ADR", "localhost:3000")
             resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
           end
         end
