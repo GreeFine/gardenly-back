081dd84fa90bc81dd815004c774929b281dbcf24
Stormy <raphael.beltrand@gmail.com>
2019-05-21 22:12:42 -0700

    add slug to garden

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,7 +1,17 @@
 class Garden < ApplicationRecord
   self.primary_key = :uuid
 
+  extend FriendlyId
+  friendly_id :slug_candidates, use: [:scoped, :slugged], scope: :user
+
   validates_uniqueness_of :name, scope: :user
   has_many :tiles, dependent: :destroy
   belongs_to :user
+
+  def slug_candidates
+    [
+      :name,
+      [:name, "#{Garden.where(user: self.user, name: name).count + 1}"]
+    ]
+  end
 end
