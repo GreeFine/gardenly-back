b93537273176215dbbc34059679c03212f8d30f9
GreeFine <greefine@hotmail.fr>
2019-07-24 21:38:11 -0700

    CreateRoom takes users

--- app/graphql/mutations/create_room.rb
+++ app/graphql/mutations/create_room.rb
@@ -2,14 +2,18 @@
 
 class Mutations::CreateRoom < Mutations::BaseMutation
   argument :name, String, required: true
+  argument :users, [ID], required: true
 
   field :room, Types::RoomType, null: true
 
   def resolve(arguments)
     return GraphQL::ExecutionError.new('User not connected') if context[:current_user].nil?
 
-    room = Room.new(arguments)
+    room = Room.new(name: arguments[:name])
     room.users.push(context[:current_user])
+    for user in arguments[:users]
+      room.users.push(User.find(user))
+    end
 
     # TODO: raise Pundit::NotAuthorizedError unless roomPolicy.new(context[:current_user], room).create?
 
